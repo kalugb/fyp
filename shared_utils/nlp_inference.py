2978fcae43d404200f1cb3ab7e533bd152c0d8f5
Blockeye <danielysj790@gmail.com>
2026-02-18 23:24:50 +0800

    Add images visualization after model testing

--- shared_utils/nlp_inference.py
+++ shared_utils/nlp_inference.py
@@ -1,12 +1,13 @@
 import torch
 import torch.nn.functional as F
-
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import json
-
 import numpy as np
-
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
+import matplotlib
+matplotlib.use("Agg")
 
 working_dir = os.getcwd()
 saved_model_dir = os.path.join(working_dir, "saved_model_params")
@@ -31,6 +32,69 @@ tokenizer = AutoTokenizer.from_pretrained(token_utils_path, use_fast=True)
 model = AutoModelForSequenceClassification.from_pretrained(finbert_utils_path, num_labels=3).to(device)
 
 model.eval()
+
+def save_graph(y, output_probs):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from sklearn.metrics import precision_recall_curve, roc_curve, auc, average_precision_score
+    from sklearn.preprocessing import LabelBinarizer
+    
+    # PRC curve
+    lb = LabelBinarizer()
+    y_test_binarized = lb.fit_transform(y)
+    classes = ["Negative", "Neutral", "Positive"]
+    n_classes = len(classes)
+    
+    precision, recall, average_precision = dict(), dict(), dict()
+    threshold = dict()
+    
+    for i in range(n_classes):
+        precision[i], recall[i], threshold[i] = precision_recall_curve(y_test_binarized[:, i], output_probs[:, i])
+        average_precision[i] = average_precision_score(y_test_binarized[:, i], output_probs[:, i])
+        
+        plt.plot(recall[i], precision[i], label=f"{classes[i]} (AP = {average_precision[i]:.2f})")
+    
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.title("Multi-clas PRC curve")
+    plt.legend(loc="lower left")
+    plt.grid(True)
+    plt.savefig("website/static/images/model_testing_nlp/prc_curve.jpg", dpi=300, bbox_inches="tight")
+    plt.close()
+    
+    # ROC curve
+    fpr, tpr, roc_auc = dict(), dict(), dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test_binarized[:, i], output_probs[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+        
+        plt.plot(fpr[i], tpr[i], label=f"{classes[i]} (AUC = {roc_auc[i]:.2f})")
+    
+    plt.plot([0, 1], [0, 1], "k--")    
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC curve for all classes")
+    plt.legend(loc="lower left")
+    plt.grid(True)
+    plt.savefig("website/static/images/model_testing_nlp/roc_curve.jpg", dpi=300, bbox_inches="tight")
+    plt.close()
+    
+    # Threshold tuning graph for class 0 and 1 (tuned class)
+    f1_negative = 2 * (precision[0][:-1] * recall[0][:-1]) / (precision[0][:-1] + recall[0][:-1])
+    f1_neutral = 2 * (precision[1][:-1] * recall[1][:-1]) / (precision[1][:-1] + recall[1][:-1])
+    plt.plot(threshold[0], precision[0][:-1], "-", color="red", label="Precision for Negative")
+    plt.plot(threshold[0], recall[0][:-1], "-", color="orange", label="Recall for Negative")
+    plt.plot(threshold[0], f1_negative, "-", color="yellow", label="F1 for Negative")
+    plt.plot(threshold[1], precision[1][:-1], "-", color="green", label="Precision for Neutral")
+    plt.plot(threshold[1], recall[1][:-1], "-", color="blue", label="Recall for Neutral")
+    plt.plot(threshold[1], f1_neutral, "-", color="indigo", label="F1 for Neutral")
+    plt.xlabel("Threshold")
+    plt.ylabel("Scoring")
+    plt.title("Scoring at various threshold for class Negative and Neutral")
+    plt.legend(loc="lower right")
+    plt.grid(True)
+    plt.savefig("website/static/images/model_testing_nlp/threshold_tuning_class0_class1.jpg", dpi=300, bbox_inches="tight")
+    plt.close()
 
 def predict_sentiment(text):
     encoding = tokenizer(text, **tokenizer_params)
@@ -80,6 +144,7 @@ def test_nlp_model(df, threshold_tuned=True, tune_class=0, fallback_class=1, unt
     
     y_pred = []
     y_true = []
+    output_probs = []
     with torch.no_grad():
         for batch_input_ids, batch_attention_mask, batch_labels in dataloader:
             batch_input_ids = batch_input_ids.to(device)
@@ -87,10 +152,9 @@ def test_nlp_model(df, threshold_tuned=True, tune_class=0, fallback_class=1, unt
             batch_labels = batch_labels.to(device)
             
             output = model(input_ids=batch_input_ids, attention_mask=batch_attention_mask)
+            probabilities = F.softmax(output.logits, dim=1)
             
             if threshold_tuned:
-                probabilities = F.softmax(output.logits, dim=1)
-                
                 for pred, true in zip(probabilities, batch_labels):
                     if torch.argmax(pred) == untuned_class:
                         label = untuned_class
@@ -99,30 +163,37 @@ def test_nlp_model(df, threshold_tuned=True, tune_class=0, fallback_class=1, unt
                 
                     y_pred.append(label)
                     y_true.append(true.cpu().item())
+                    output_probs.append(pred.detach().cpu().numpy())
             else:
                 predictions = torch.argmax(output.logits, dim=1)
                 
                 y_pred.append(predictions.cpu())
                 y_true.append(batch_labels.cpu())    
+                output_probs.append(probabilities.detach().cpu().numpy())
     
     y_pred = y_pred if threshold_tuned else torch.cat(y_pred).numpy()
     y_true = y_true if threshold_tuned else torch.cat(y_true).numpy()
+    raw_y = y.detach().cpu().numpy()
+    output_probs = np.array(output_probs)
     
     confusion = confusion_matrix(y_true, y_pred)
+    
+    save_graph(raw_y, output_probs)
     
     return confusion
        
 if __name__ == "__main__": 
-    # print(predict_sentiment("Intel stock plunges as hopes for a 'clean' turnaround story meet reality"))
+    matplotlib.use("TkAgg")
+    print(predict_sentiment("Intel stock plunges as hopes for a 'clean' turnaround story meet reality"))
     
     import pandas as pd
     csv_dir = os.path.join(os.getcwd(), "csv_files")
     
     df = pd.read_csv(os.path.join(csv_dir, "raw", "enhanced_phrasebank.csv"), index_col=0)
     
-    r, c = test_nlp_model(df, threshold_tuned=False)
-    print(r, c)
-    print(type(r), type(c))    
+    c = test_nlp_model(df, threshold_tuned=True)
+    print(c)
+    print(type(c))    
     
     
     
--- shared_utils/num_inference.py
+++ shared_utils/num_inference.py
@@ -2,10 +2,101 @@ import pandas as pd
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+import matplotlib
+matplotlib.use("Agg")
 
 from shared_utils.load_utils import load_num_model
 
 model = load_num_model()
+
+def save_graph(X, y, output_probs):
+    from sklearn.metrics import precision_recall_curve, average_precision_score, roc_curve, auc
+    from sklearn.model_selection import learning_curve
+    from sklearn.preprocessing import LabelBinarizer
+    from sklearn.linear_model import LogisticRegression
+    import matplotlib.pyplot as plt
+    plt.figure(figsize=(8, 6))
+    import json
+    import numpy as np
+    
+    # PRC curve
+    lb = LabelBinarizer()
+    y_test_binarized = lb.fit_transform(y)
+    classes = ["Short", "Hold", "Long"]
+    n_classes = len(classes)
+    
+    precision, recall, average_precision = dict(), dict(), dict()
+    
+    for i in range(n_classes):
+        precision[i], recall[i], _ = precision_recall_curve(y_test_binarized[:, i], output_probs[:, i])
+        average_precision[i] = average_precision_score(y_test_binarized[:, i], output_probs[:, i])
+        
+        plt.plot(recall[i], precision[i], label=f"{classes[i]} (AP = {average_precision[i]:.2f})")
+        
+    plt.xlabel("Recall")
+    plt.ylabel("Precision")
+    plt.title("Multi-class PRC curve")
+    plt.legend(loc="lower left")
+    plt.grid(True)
+    plt.savefig("website/static/images/model_testing_num/prc_curve.jpg", dpi=300, bbox_inches="tight")
+    plt.close()
+    
+    # ROC curve
+    fpr, tpr, roc_auc = dict(), dict(), dict()
+    for i in range(n_classes):
+        fpr[i], tpr[i], _ = roc_curve(y_test_binarized[:, i], output_probs[:, i])
+        roc_auc[i] = auc(fpr[i], tpr[i])
+        
+        plt.plot(fpr[i], tpr[i], label=f"{classes[i]} (AUC = {roc_auc[i]:.2f})")
+        plt.plot([0, 1], [0, 1], "k--")
+       
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("ROC curve for all classes")
+    plt.legend(loc="lower left")
+    plt.grid(True)
+    plt.savefig("website/static/images/model_testing_num/roc_curve.jpg", dpi=300, bbox_inches="tight")
+    plt.close()
+    
+    # learning curve
+    with open("saved_model_params/json_files/num_config.json", "r") as file:
+        data = json.load(file)
+        
+        parameters = data["params"]
+        max_iter = parameters["lr__max_iter"]
+        C = parameters["lr__C"]
+        class_weight = {float(k): v for k, v in parameters["class_weight"].items()}
+        
+    using_model = LogisticRegression(max_iter=max_iter, C=C, class_weight=class_weight,
+                               verbose=0, random_state=67, n_jobs=-1)
+        
+    train_size, train_scores, val_scores = learning_curve(
+        using_model, X, y,
+        train_sizes=np.linspace(0.2, 1.0, 5),
+        cv=3,
+        scoring="precision_macro",
+        shuffle=True,
+        random_state=67,
+        n_jobs=-1
+    )
+    train_mean = np.mean(train_scores, axis=1)
+    train_std = np.std(train_scores, axis=1)
+    val_mean = np.mean(val_scores, axis=1)
+    val_std = np.std(val_scores, axis=1)
+     
+    plt.plot(train_size, train_mean, "o-", color="blue", label="Training Score")
+    plt.fill_between(train_size, train_mean + train_std, train_mean - train_std, alpha=0.15, color="blue")
+    
+    plt.plot(train_size, val_mean, "o-", color="green", label="Validation Score")
+    plt.fill_between(train_size, val_mean + val_std, val_mean - val_std, alpha=0.15, color="green")
+    
+    plt.xlabel("Training Set Size")
+    plt.ylabel("Score")
+    plt.title("Learning Curve")
+    plt.legend(loc="best")
+    plt.grid(True)
+    plt.savefig("website/static/images/model_testing_num/learning_curve.jpg", dpi=300, bbox_inches="tight")
+    plt.close()
 
 def predict_position(adj_close, returns, ma, ratio):
     data = [[adj_close, returns, ma, ratio]] 
@@ -33,7 +124,7 @@ def predict_position(adj_close, returns, ma, ratio):
     return position, int(position_label), probabilities.tolist()
 
 def test_num_model(df, perform_mean_reversion=True):
-    from sklearn.metrics import confusion_matrix, classification_report
+    from sklearn.metrics import confusion_matrix
     
     if perform_mean_reversion:
         from website.backend_functions import mean_reversion
@@ -52,8 +143,11 @@ def test_num_model(df, perform_mean_reversion=True):
     X = df.drop(columns="position")
     y = df["position"]
     output_list = model.predict(X)
+    output_probs = model.predict_proba(X)
     
     confusion = confusion_matrix(y, output_list)
+    
+    save_graph(X, y, output_probs)
     
     return confusion
     
@@ -64,9 +158,9 @@ if __name__ == "__main__":
     df_dir = os.path.join(os.getcwd(), "csv_files", "raw", "historical_stock_data.csv")
     df = pd.read_csv(df_dir, index_col=0)
 
-    r, c = test_num_model(df)
+    c = test_num_model(df)
     
-    print(r, c)
-    print(type(r), type(c))
+    print(c)
+    print(type(c))
 
 
